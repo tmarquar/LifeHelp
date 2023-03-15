165274d6f0e2d1c47957e7afbb2967c858068e81
tmarquard <tmarquard@10xpeople.com>
2023-03-15 17:42:35 +0000

    starting the new direction

--- src/components/header.js
+++ src/components/header.js
@@ -1,1 +1,28 @@
 // Create a basic Header for the page that will allow user to navigate from Home to Settings
+// and back to Home
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="App-header">
+            <div>
+                <p>
+                <span className="heart">♥️</span>
+                </p>
+                <div>Test App</div>
+            </div>
+            <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </p>
+        </header>
+    );
+    };
+
+export default Header;
